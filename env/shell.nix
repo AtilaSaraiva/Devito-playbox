73eca3303554d9db558ef854dad3b428cce17798
Átila Saraiva <atilasaraiva@gmail.com>
2022-02-25 01:17:37 -0300

    Added all examples to the shallow water equation notebook

--- env/shell.nix
+++ env/shell.nix
@@ -2,11 +2,14 @@ with (import ./inputs.nix);
 mach-nix.nixpkgs.mkShell {
   buildInputs = [
     (import ./python.nix)
+    pkgs.ffmpeg
+    pkgs.glibc
+    pkgs.llvmPackages.openmp
     #mach-nix.mach-nix
   ];
 
   shellHook = ''
     export DEVITO_LOGGING=DEBUG
-    #jupyter lab
+    jupyter lab
   '';
 }
